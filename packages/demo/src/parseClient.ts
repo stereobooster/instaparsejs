dbe3331588e90af78f10c279aca8a3eecb8fa5f4
stereobooster <stereobooster@gmail.com>
2024-07-21 20:43:10 +0200

    notes

--- packages/demo/src/parseClient.ts
+++ packages/demo/src/parseClient.ts
@@ -11,6 +11,8 @@ const instance = new ComlinkWorker<typeof import("./parseWorker")>(
 // TODO: is there a way to cancel all previous requests if there is more fresher one
 // https://github.com/GoogleChromeLabs/comlink/issues/372
 // https://github.com/whatwg/dom/issues/948
+// https://vitejs.dev/guide/features.html#web-workers
+// https://developer.mozilla.org/en-US/docs/Web/API/Worker/terminate
 function _parseClient(grammar: string, text: string) {
   return instance.parseWorker(grammar, text);
 }
