e7b78c0aa0f81e7b2c3889e133931ba1ea7af979
stereobooster <stereobooster@gmail.com>
2024-08-17 14:07:36 +0200

    v0.0.5

--- packages/instaparsejs/index.js
+++ packages/instaparsejs/index.js
@@ -4,9 +4,9 @@ import {
   parserPosAll as parserPosAllOrig,
 } from "./dist/main.js";
 
-function transformTree(tree, n, siblings) {
+function transformTree(tree, n, siblings, parentPos, total) {
   if (tree.value != undefined) {
-    if (n !== undefined && siblings !== undefined) {
+    if (n != undefined && siblings != undefined) {
       let start = null;
       let end = null;
       const prev = siblings[n - 1];
@@ -14,10 +14,23 @@ function transformTree(tree, n, siblings) {
 
       if (prev && prev.pos) start = prev.pos[1];
       if (next && next.pos) end = next.pos[0];
+
       if (start == null && end != null) start = end - [...tree.value].length;
+
+      // unfortunately this may give skewed positions when there are "hidden" nodes in siblings
+      // like,  `E <"&"> E`
+      if (start == null && n == 0 && parentPos != undefined)
+        start = parentPos[0];
+
       if (start != null && end == null) end = start + [...tree.value].length;
 
+      // unfortunately this may give skewed positions when there are "hidden" nodes in siblings
+      // like,  `E <"&"> E`
+      if (end == null && n == total - 1 && parentPos != undefined)
+        end = parentPos[1];
+
       tree.pos = [start, end];
+
       if (start == null || end == null) return 0;
     }
   } else {
@@ -26,13 +39,21 @@ function transformTree(tree, n, siblings) {
       delete tree.children;
     } else {
       const checkLength = tree.children
-        .map((x, i) => transformTree(x, i, tree.children))
+        .map((x, i) =>
+          transformTree(x, i, tree.children, tree.pos, tree.children.length)
+        )
         .reduce((x, y) => x + y, 0);
       if (checkLength !== tree.children.length) {
         [...tree.children]
           .reverse()
           .forEach((x, i) =>
-            transformTree(x, tree.children.length - i - 1, tree.children)
+            transformTree(
+              x,
+              tree.children.length - i - 1,
+              tree.children,
+              tree.pos,
+              tree.children.length
+            )
           );
       }
     }
